f08294d4090c89825a10ee1cf256c688602e731b
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-20 07:12:50 +0000

    Append globals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const bowlby = localFont({
+  src: '../../public/fonts/BowlbyOneSC-Regular.ttf',
+  variable: '--font-bowlby'
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const dmMono = localFont({
+  src: '../../public/fonts/DMMono-Regular.ttf',
+  variable: '--font-dmMono'
+})
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      className={`${bowlby.variable} ${dmMono.variable} antialiased`}
       >
         {children}
       </body>
