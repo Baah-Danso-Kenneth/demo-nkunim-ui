4e4a117d20ae3bfb8673fe6968e11929efed27c8
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-21 08:01:19 +0000

    Update codebase - Added a new fonts to suite a particular purpose - Add a background texture in the global.css - Specify color for codebase in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,10 +15,14 @@ export default {
       fontFamily: {
         bowlby: "var(--font-bowlby)",
         dmMono: "var(--font-dmMono)",
+        playWright: "var(--font-playWright)",
       },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "brand-light": "#E4E1DC",
+        "brand-beige": "#BBAA89",
+        "brand-beige-hot": "#B29B7E",
+        "brand-hot-red": "#FE191F",
+        "brand-base-color": "#cbc8b8"
       },
     },
     screens,
