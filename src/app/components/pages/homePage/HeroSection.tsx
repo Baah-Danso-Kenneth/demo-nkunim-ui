67c9f719d0c178eab318c1726f01b27dd2b92664
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-21 08:03:12 +0000

    Create Hero Section UI

---
+++ src/app/components/pages/homePage/HeroSection.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Bounded from '../../shared/Bounded'
+import { Heading } from '../../shared/Heading'
+
+function HeroSection() {
+  return (
+    <Bounded 
+    className='relative bg-brand-base-color h-dvh bg-texture overflow-hidden text-zinc-800'>
+        <div className='absolute inset-0 
+        mx-auto mt-24 grid max-w-6xl grid-rows-[1fr,auto]
+        place-items-end px-6 ~py-10/16
+        '>
+        <Heading className='font-bowlby place-self-start' as='h1'>
+             Pay everythng with ease and breeze
+        </Heading>
+
+        <div className='flex relative w-full justify-between flex-col items-center ~gap-2/4 lg:flex-row'>
+            <p className='font-dmMono max-w-[45ch] font-semibold ~text-lg/xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nulla, facilis, assumenda id numquam excepturi quam maxime illo quibusdam explicabo libero 
+            ducimus aspernatur doloremque 
+            beatae soluta perferendis ut laboriosam nesciunt.</p>
+
+            <div>
+            <button>get started</button>
+            </div>
+            
+        </div>
+
+        </div>
+    </Bounded>
+  )
+}
+
+export default HeroSection
