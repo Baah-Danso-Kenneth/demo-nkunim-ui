d748f46b36ce84b8706782772f6a41def96d7700
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-23 08:18:11 +0000

    Raw data to be consume locally

--- src/app/data.js
+++ src/app/data.js
@@ -1,5 +1,12 @@
  import { LuArrowDownRight } from "react-icons/lu";
 
+ import { HiClipboardList, HiCurrencyDollar } from "react-icons/hi";
+ import { HiUser } from "react-icons/hi";
+ import { HiOutlineCurrencyDollar } from "react-icons/hi";
+ import { HiOutlineDesktopComputer } from "react-icons/hi";
+ import { HiReceiptRefund } from "react-icons/hi";
+
+
  export const sociaLinks=[
    {name:'discord', icon: <LuArrowDownRight className='h-6'/>,href:'#'},
    {name:'twitter', icon:<LuArrowDownRight className='h-6'/>,href:'#'},
@@ -46,4 +53,36 @@ export const teamMembers=[
   {firstName:'Claud', role:"blockchain developer",connect:"#", image:'/images/claud_img.jpg', lastName:'opoku'},
   {firstName:'makafui', role:"ui/ux designer",connect:"#", image:'/images/moto.jpg', lastName:'richman'},
   {firstName:'elsie', role:"project manager",connect:"#", image:'/images/base2_nft.png', lastName:'somuah'},
+]
+
+export const sidearContent=[
+  {name:'dashboard',href:"/dashboard",icon:<HiOutlineDesktopComputer/>},
+  {name:'payment',href:"/payment",icon:<HiOutlineCurrencyDollar/>},
+  {name:'history',href:"/history",icon:<HiReceiptRefund/>},
+  {name:'profile',href:"/profile",icon:<HiUser/>},
+  {name:'documents',href:"/documents",icon:<HiClipboardList/>},
+]
+
+
+export const transactions=[
+  {walletAddress:'0FXX0003255555581249999',date:'25 Jan 10:28:14 AM',completed:'Completed',ratings:'-$13000'},
+  {walletAddress:'0FXX0003255555581249999',date:'25 Jan 10:28:14 AM',completed:'Completed',ratings:'-$13000'},
+  {walletAddress:'0FXX0003255555581249999',date:'25 Jan 10:28:14 AM',completed:'Completed',ratings:'-$13000'},
+  {walletAddress:'0FXX0003255555581249999',date:'25 Jan 10:28:14 AM',completed:'Completed',ratings:'-$13000'},
+  {walletAddress:'0FXX0003255555581249999',date:'25 Jan 10:28:14 AM',completed:'Completed',ratings:'-$13000'},
+  {walletAddress:'0FXX0003255555581249999',date:'25 Jan 10:28:14 AM',completed:'Completed',ratings:'-$13000'},
+  {walletAddress:'0FXX0003255555581249999',date:'25 Jan 10:28:14 AM',completed:'Completed',ratings:'-$13000'},
+  {walletAddress:'0FXX0003255555581249999',date:'25 Jan 10:28:14 AM',completed:'Completed',ratings:'-$13000'},
+  {walletAddress:'0FXX0003255555581249999',date:'25 Jan 10:28:14 AM',completed:'Completed',ratings:'-$13000'},
+  {walletAddress:'0FXX0003255555581249999',date:'25 Jan 10:28:14 AM',completed:'Completed',ratings:'-$13000'},
+]
+
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
 ]
