3205998926977ba2ad2a0bcf23a93c773eee684b
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-25 03:58:16 +0000

    Fix files bugs

--- src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import DefaultLoader from './../components/content/DefaultLoader'
-
-function Loading() {
-  return <DefaultLoader/>
-}
-
-export default Loading
--- src/app/profile/page.tsx
+++
@@ -1,8 +0,0 @@
-import ProfileContent from '@/components/pages/profile'
-import React from 'react'
-
-function page() {
-  return <ProfileContent/>
-}
-
-export default page
