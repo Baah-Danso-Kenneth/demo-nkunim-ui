a681054274e45f7bf3b24702cf7ed246ab13d700
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-28 08:46:15 +0000

    Next js generated files

--- src/components/pages/history/index.tsx
+++ src/components/pages/history/index.tsx
@@ -19,87 +19,88 @@ function History() {
     });
 
   return (
-    <div className='h-full'>
-        <HistoryHeader/>
-
-        <div ref={ref} className='w-[75%] mx-auto relative mt-20'>
+    <div>
+  
+        <div ref={ref} className='w-[75%] mx-auto relative mt-10'>
             <motion.div
             style={{scaleY:scrollYProgress}}
              className='absolute left-9 top-0 w-[4px] h-full bg-black origin-top'/>
 
             <ul className='flex flex-col justify-between items-start w-full ml-4'>
                 <HistoryDetails items={{
-                      time:'2025-present',
-                      position:'Softwear engineer',
+                      deliveryType:'send',
+                      time:'2hours ago',
                       address:'Ohio Oklahoma',
+                      amount:'180',
                       work:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quod eos cumque praesentium in veniam corporis esse minima voluptatem quos. ",
                       href: '#',
-                     company:'google'
+                      stats:true,
+                      from:'0XFFFF00000',
+                      to:'0XFF32200',
                   }}/>
+
+                  <HistoryDetails items={{
+                      deliveryType:'receive',
+                      time:'2hours ago',
+                      address:'Ohio Oklahoma',
+                      amount:'180',
+                      work:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quod eos cumque praesentium in veniam corporis esse minima voluptatem quos. ",
+                      href: '#',
+                      stats:true,
+                      from:'0XFFFF00000',
+                      to:'0XFF32200',
+                  }}/>
+
 
                 <HistoryDetails items={{
-                      time:'2025-present',
-                      position:'Softwear engineer',
+                      deliveryType:'send',
+                      time:'2hours ago',
                       address:'Ohio Oklahoma',
+                      amount:'180',
                       work:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quod eos cumque praesentium in veniam corporis esse minima voluptatem quos. ",
                       href: '#',
-                     company:'google'
+                      stats:true,
+                      from:'0XFFFF00000',
+                      to:'0XFF32200',
                   }}/>
 
 
-                    <HistoryDetails items={{
-                      time:'2025-present',
-                      position:'Softwear engineer',
+                <HistoryDetails items={{
+                      deliveryType:'convert',
+                      time:'2hours ago',
                       address:'Ohio Oklahoma',
+                      amount:'180',
                       work:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quod eos cumque praesentium in veniam corporis esse minima voluptatem quos. ",
                       href: '#',
-                     company:'google'
+                      stats:true,
+                      from:'0XFFFF00000',
+                      to:'0XFF32200',
+                  }}/>
+                <HistoryDetails items={{
+                      deliveryType:'send',
+                      time:'2hours ago',
+                      address:'Ohio Oklahoma',
+                      amount:'180',
+                      work:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quod eos cumque praesentium in veniam corporis esse minima voluptatem quos. ",
+                      href: '#',
+                      stats:true,
+                      from:'0XFFFF00000',
+                      to:'0XFF32200',
                   }}/>
 
-<HistoryDetails items={{
-                      time:'2025-present',
-                      position:'Softwear engineer',
+              <HistoryDetails items={{
+                      deliveryType:'send',
+                      time:'2hours ago',
                       address:'Ohio Oklahoma',
+                      amount:'180',
                       work:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quod eos cumque praesentium in veniam corporis esse minima voluptatem quos. ",
                       href: '#',
-                     company:'google'
+                      stats:true,
+                      from:'0XFFFF00000',
+                      to:'0XFF32200',
                   }}/>
 
-<HistoryDetails items={{
-                      time:'2025-present',
-                      position:'Softwear engineer',
-                      address:'Ohio Oklahoma',
-                      work:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quod eos cumque praesentium in veniam corporis esse minima voluptatem quos. ",
-                      href: '#',
-                     company:'google'
-                  }}/>
 
-<HistoryDetails items={{
-                      time:'2025-present',
-                      position:'Softwear engineer',
-                      address:'Ohio Oklahoma',
-                      work:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quod eos cumque praesentium in veniam corporis esse minima voluptatem quos. ",
-                      href: '#',
-                     company:'google'
-                  }}/>
-
-<HistoryDetails items={{
-                      time:'2025-present',
-                      position:'Softwear engineer',
-                      address:'Ohio Oklahoma',
-                      work:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quod eos cumque praesentium in veniam corporis esse minima voluptatem quos. ",
-                      href: '#',
-                     company:'google'
-                  }}/>
-
-<HistoryDetails items={{
-                      time:'2025-present',
-                      position:'Softwear engineer',
-                      address:'Ohio Oklahoma',
-                      work:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quod eos cumque praesentium in veniam corporis esse minima voluptatem quos. ",
-                      href: '#',
-                     company:'google'
-                  }}/>
             </ul>
 
         </div>
