e4e479c6627bc38231d4bf4b4c85617b75afca98
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-31 12:54:18 +0000

    Ensure React Component starts with Caps letters to avoid bugs

--- src/app/usdConvertor/layout.tsx
+++ src/app/usdConvertor/layout.tsx
@@ -1,19 +1,27 @@
 import React, { ReactNode } from 'react'
 import Header from '@/components/layouts/Header'
 import Bounded from '@/components/shared/Bounded'
+import { ProtectedRoute } from '@/lib/ProtectedRoute'
+import { usePrivy } from '@privy-io/react-auth'
 
-function layout({children}:{children:ReactNode}) {
+
+function Layout({children}:{children:ReactNode}) {
+  const {user,logout} = usePrivy();
+  
+  const wallet = user?.wallet
+
   return (
+    <ProtectedRoute>
     <div className='flex-col bg-wineTexture'>
         <div className='h-[10vh] text-zinc-800'>
-          <Header/>
+          <Header wallet={wallet} logOut={logout}/>
         </div>
         <Bounded className='w-full  bg-softBlend'>
             {children}
         </Bounded>
-
     </div>
+    </ProtectedRoute>
   )
 }
 
-export default layout
+export default Layout
