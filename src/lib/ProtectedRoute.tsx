b4794f06f2b304f3e4cd73b20b4538e611095002
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-31 10:25:07 +0000

    Create ProtectedRoute to ensure certain pages are restricted to only authencticated users

---
+++ src/lib/ProtectedRoute.tsx
@@ -0,0 +1,25 @@
+"use client"
+import DefaultLoader from "@/components/content/DefaultLoader";
+import { usePrivy } from "@privy-io/react-auth";
+import { useRouter } from "next/navigation";
+import React, { ReactNode, useEffect } from "react";
+
+
+export const ProtectedRoute=({children}:{children:ReactNode})=>{
+    const {ready, authenticated} = usePrivy();
+    const router = useRouter();
+
+    useEffect(()=>{
+        if(ready && !authenticated){
+            router.push('/');
+        }
+    },[ready, authenticated])
+
+    if(ready && authenticated){
+        return<>{children}</>
+    }
+
+
+    return <DefaultLoader/>
+
+}
